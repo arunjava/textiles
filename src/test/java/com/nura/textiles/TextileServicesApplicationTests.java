5f4e954fb38f31606818298ed2c9984e3d47e52e
Arun Kumar <aprodigalboy@gmail.com>
2022-01-30 12:18:02 +0530

    Initial commit to master

---
+++ src/test/java/com/nura/textiles/TextileServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nura.textiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextileServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
